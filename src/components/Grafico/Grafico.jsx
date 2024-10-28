f115da8caf7a64d2a5d1fa1e9ae38f1d97625f47
igor <igorsoosrribeiro@gmail.com>
2024-10-28 11:47:24 -0300

    criando os graficos

---
+++ src/components/Grafico/Grafico.jsx
@@ -0,0 +1,31 @@
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
+export default function Grafico({data}) {
+    
+ 
+    return (
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeDasharray="5 5" />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+  }
+
