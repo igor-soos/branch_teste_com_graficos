4c6548240f820ec8b60a768414a4a51eaa6d5782
igor <igorsoosrribeiro@gmail.com>
2024-10-28 10:57:20 -0300

    criando a estilização da sidebar

--- src/components/Nav/Side.jsx
+++ src/components/Nav/Side.jsx
@@ -3,10 +3,17 @@ import {Sidebar,Menu,MenuItem,SubMenu,sidebarClasses} from 'react-pro-sidebar'
 export default function Side() {
   return (
     <Sidebar>
+
+        rootStyles={{
+            ['.$(sidebarClasses.container']:{
+                backgroundColor:'green',color:'blue'
+            }
+        }}
         <Menu>
           <SubMenu label="Dasboard">
-            <MenuItem>Teste</MenuItem>
-            <MenuItem>Teste-2</MenuItem>
+            <MenuItem component={<Link to='/'/>}>Home</MenuItem>
+            <MenuItem component={<Link to='/Exemplo1'/>}>Exemplo-1</MenuItem>
+            <MenuItem component={<Link to='/Exemplo2'/>}>Exemplo-2</MenuItem>
           </SubMenu>
           <SubMenu label="Conta">
             <MenuItem>Minha Conta</MenuItem>
